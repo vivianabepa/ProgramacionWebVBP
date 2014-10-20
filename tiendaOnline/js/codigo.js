b1778962a1a15de16cc79c89c6cdda28bc20a25a
vivianabepa <vivianabenavides@Vivs-MacBook-Pro.local>
2014-10-20 12:59:46 -0500

    qqqq
    qqqqq

--- tiendaOnline/js/codigo.js
+++
@@ -1,8 +0,0 @@
-$(document).ready(inicio)
-function inicio(){
-	$(".botoncompra").click(anade)
-	$("#carrito").load("php/poncarrito.php");
-}
-function anade () {
-	$("#carrito").load("php/poncarrito.php?p="+$(this).val());
-}
